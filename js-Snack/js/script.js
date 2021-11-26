bbb8e025083b0a7c28d2b832929e6944ee55eea7
Dandav2923 <daniele.emili23@gmail.com>
2021-11-26 14:55:27 +0100

    init

---
+++ js-Snack/js/script.js
@@ -0,0 +1,24 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// primo passaggio chiedere all'utente per 10 volte di inserire un numero random
+// poi fare la somma tra i numeri inseriti
+
+// svolgimento con ciclo for 
+// let somma = 0;
+// for (let index = 9; index >=0 ; index--) {
+//     const numberUser = parseInt (prompt ('Scrivi un numero'));
+//     somma += numberUser;
+//     console.log(somma);
+// }
+
+// svolgimento con ciclo while 
+let somma = 0;
+let contatore = 9;
+
+while (contatore >= 0) {
+    const numberUser = parseInt(prompt('Scrivi un numero'));
+    somma += numberUser;
+    console.log(somma);
+    contatore--;
+}
