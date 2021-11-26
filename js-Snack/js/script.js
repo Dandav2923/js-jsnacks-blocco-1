be7c10602fda0aae76717c0474af30f767a662cb
Dandav2923 <daniele.emili23@gmail.com>
2021-11-26 16:50:44 +0100

    fine esercizio

--- js-Snack/js/script.js
+++ js-Snack/js/script.js
@@ -28,31 +28,57 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 // script con ciclo for 
+// const nameUser = prompt('Inserisci il tuo nome');
+// const nameList = [
+//  'daniele',
+//  'chiara',
+//  'fulvio',
+//  'meme',
+//  'sara',
+//  'valerio',
+//  'christian',
+//  'fabio',
+//  'francesco'
+// ];
+
+// let boolean = false;
+// for (let counter = nameList.length - 1; counter >= 0; counter--) {
+//     if (nameUser == nameList[counter]) { 
+//         boolean = true;
+//     }
+// }
+// console.log(boolean);
+// if (boolean == true) {
+//     console.log('Sei nella lista puoi entrare');
+// } 
+// else 
+//     console.log('Non sei nella lista non puoi entrare');
+
+// script con il ciclo while 
+
 const nameUser = prompt('Inserisci il tuo nome');
 const nameList = [
- 'daniele',
- 'chiara',
- 'fulvio',
- 'meme',
- 'sara',
- 'valerio',
- 'christian',
- 'fabio',
- 'francesco'
+    'daniele',
+    'chiara',
+    'fulvio',
+    'meme',
+    'sara',
+    'valerio',
+    'christian',
+    'fabio',
+    'francesco'
 ];
-let counter = nameList.length-1;
-// console.log(counter);
 let boolean = false;
-for (let counter; counter >= 0; counter--) {
-    if (nameList[counter] == nameUser) { 
+let counter = nameList.length -1;
+
+while (counter >= 0) {
+    if (nameUser == nameList[counter]) {
         boolean = true;
     }
+    counter--;
 }
 if (boolean == true) {
     console.log('Sei nella lista puoi entrare');
-} 
+}
 else 
     console.log('Non sei nella lista non puoi entrare');
-
-
-
