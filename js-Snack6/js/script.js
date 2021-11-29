b1fd540cad9d6d5c1169643348cfef45bed9c1f8
Dandav2923 <daniele.emili23@gmail.com>
2021-11-29 12:39:19 +0100

    decremento

--- js-Snack6/js/script.js
+++ js-Snack6/js/script.js
@@ -3,7 +3,7 @@
 // dove N è un numero indicato dall’utente.
 
 const numberUser = parseInt(prompt('Inserisci i numeri di cui vuoi sapere il valore elevato al cubo'));
-for (let i = 0; i < numberUser; i++) {
+for (let i = numberUser; i >= 0; i--) {
     const cubo = Math.pow(i, 3);
     console.log(cubo);
 }
