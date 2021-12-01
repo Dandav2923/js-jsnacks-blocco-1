7f2055a9d4288e28921348c1ef9973d8af4f9295
Dandav2923 <daniele.emili23@gmail.com>
2021-12-01 13:02:20 +0100

    snack 3

---
+++ js-snack3/js/script.js
@@ -0,0 +1,24 @@
+// alert ('ciao');
+// Crea due array che hanno un numero di elementi diversi.
+//  Aggiungi elementi all’array che ha meno elementi
+//  fino a quando ne avrà tanti quanti l’altro.
+
+// creiamo i 2 array 
+let lista1 = [1,2,3];
+let lista2 = [1,2,3,4,5,6,7];
+
+let lunghezzaLista1 = lista1.length;
+let lunghezzaLista2 = lista2.length;
+
+let counter = 0;
+while (!(lunghezzaLista1 == lunghezzaLista2)) {
+    if (lunghezzaLista1 > lunghezzaLista2) {
+        lista2.push(lista2[lunghezzaLista2 -1]+1);
+    } else if (lunghezzaLista2 > lunghezzaLista1) {
+        lista1.push(lista1[lunghezzaLista1 - 1] + 1);
+    }
+
+}
+
+console.log(lista1);
+console.log(lista2);
